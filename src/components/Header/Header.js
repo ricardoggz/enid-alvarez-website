d5a6abbab183e95218db7b2f6416f0469a9ffe2d
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-20 13:13:35 -0600

    se agrego el header falta responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 'use client'
-import './header.module.css'
+import Link from 'next/link'
+import styles from './header.module.css'
 
 export const Header = ()=>{
     return(
-        <header>
-            <nav>
-                <figure>
+        <header className={styles.headerWrapper}>
+            <nav className={`flexContainer ${styles.navElements}`}>
+                <figure className={styles.navImage}>
+                    <Link href='/'>
                     <img src='logo-mediano-grande.jpg' />
+                    </Link>
                 </figure>
+                <ul className={styles.navList}>
+                    <Link href='/'>
+                        <li>Inicio</li>
+                    </Link>
+                </ul>
             </nav>
         </header>
     )

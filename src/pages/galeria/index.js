fb2d36160389368e622f7eb17fb7fb2de5661d94
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-20 17:01:24 -0600

    pagina de galeria creada

---
+++ src/pages/galeria/index.js
@@ -0,0 +1,12 @@
+import { Container, RootLayout } from "@/components";
+export default function Gallery(){
+    return(
+        <RootLayout>
+            <Container>
+                <h1 className="title">
+                    Bolsos disponibles
+                </h1>
+            </Container>
+        </RootLayout>
+    )
+}

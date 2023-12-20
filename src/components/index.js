572212061e169e112f89cebc1a59f56ef81bc8bf
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-20 13:23:17 -0600

    se agrego un componente para las secciones

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './Header/Header'
 export * from './RootLayout/RootLayout'
+export * from './Container/Container'
--- src/pages/pago-producto/index.js
+++ src/pages/pago-producto/index.js
@@ -1,10 +1,12 @@
 import '../../globals.css'
 import { SelectProduct } from './components/SelectProduct/SelectProduct'
-import { RootLayout } from '@/components'
+import { RootLayout, Container } from '@/components'
 export default function Gallery(){
     return(
         <RootLayout>
-            <SelectProduct />
+            <Container>
+                <SelectProduct />
+            </Container>
         </RootLayout>
     )
 }

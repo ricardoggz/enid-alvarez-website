2fa941ec7f2fb0eb98f38460fcb111961596f97e
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-21 13:49:45 -0600

    se agregaron las fotos relacionadas del producto

--- src/pages/pago-producto/components/SelectProduct/SelectProduct.js
+++ src/pages/pago-producto/components/SelectProduct/SelectProduct.js
@@ -7,7 +7,6 @@ import styles from './selectProduct.module.css'
 const SelectProduct = ({products}) => {
   // Estado para la imagen actual del producto
   const [currentProduct, setCurrentProduct] = useState(products[0]);
-
   // Función para cambiar la imagen según el color seleccionado
   const handleColorChange = (color) => {
     const selectedProduct = products.find((product) => product.color === color);
@@ -15,10 +14,29 @@ const SelectProduct = ({products}) => {
       setCurrentProduct(selectedProduct);
     }
   };
+  let images
+  if(products){
+    images = JSON.parse(products[0].images)
+    console.log(images)
+  }
   return (
     <div className={styles.mainContainer}>
       <div className={styles.productImage}>
       <img src={currentProduct.product_image} alt="Producto"/>
+      <div className={styles.relatedImages}>
+        {
+          products ?
+          images.map((image)=>(
+            <img
+              src={image.imagen_ruta}
+              alt="Producto"
+              key={image.imagen_id}
+              />
+          ))
+          :
+          null
+        }
+      </div>
       </div>
       <div className={styles.productDescription}>
       <h1>Modelo: {currentProduct.product_name}</h1>

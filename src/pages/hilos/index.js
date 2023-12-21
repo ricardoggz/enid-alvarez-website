ec6ed1c49647e3dacbaae26d81c11e549fbb17a7
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-21 15:42:55 -0600

    se agrega el consumo de la api en pagina de hilos

--- src/pages/hilos/index.js
+++ src/pages/hilos/index.js
@@ -1,14 +1,23 @@
 import { products } from "./consts";
+import { useFetch } from "@/hooks";
 import { Container, RootLayout, GridContainer } from "@/components";
 
 export default function Threads(){
+    const [loading, data] = useFetch({
+        url: 'http://localhost:3030/api/products/all-products'
+    })
+    /*Existe un filtrado para bolsos*/
+    let filteredData
+    if(data){
+        filteredData = data.data.filter((product)=> product.category_id === 2)
+    }
     return(
         <RootLayout>
             <Container>
                 <h1 className="title">
                     Hilos disponibles
                 </h1>
-                <GridContainer products={products}/>
+                <GridContainer products={filteredData}/>
             </Container>
         </RootLayout>
     )
